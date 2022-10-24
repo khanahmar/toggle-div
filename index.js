b75b65854b13ad732f81dc05e36356646723abdb
Ahmar khan <ahmarbilal223344@gmail.com>
2022-10-24 15:15:07 +0500

    changes made

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const div = Array.from(document.getElementsByClassName("div"));
+let h1 = document.getElementById("h1");
 
+let random;
 div.forEach((divs) => {
   divs.addEventListener("click", (e) => {
-    divs.classList.toggle("change");
+    divs.classList.add("change");
+    random = divs.innerHTML = Math.floor(Math.random() * 10);
+    if (random === 0) {
+      h1.innerHTML = "You have lost the Game!";
+      h1.classList.add("h1");
+    }
   });
 });
